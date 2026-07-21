69a8883aa6cb6deb83b18d74bcb0d18f238d545a
Ayush Baranwal <baranwalayush25@gmail.com>
2026-07-21 23:06:58 +0530

    feat: initialize backend structure with Spring Boot and Maven wrapper

---
+++ backend/src/test/java/com/readypath/ReadypathApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readypath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadypathApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
